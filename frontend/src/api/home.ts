3d2a3c684bea062e3feccede5f3bf8ccdd23b0ba
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2025-03-20 22:09:44 +0100

    Fetch secured details from home page.

---
+++ frontend/src/api/home.ts
@@ -0,0 +1,3 @@
+import { axiosInstance } from "./api";
+
+export const fetchHome = async () => axiosInstance.get("home/");
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import { AuthData } from "../auth/AuthWrapper";
+import { fetchHome } from "../api/home";
 
 export const Home = () => {
   const { user, logout } = AuthData();
+
+  const [homeDetails, setHomeDetails] = useState("");
+
+  const fetchDetails = async () => {
+    fetchHome().then((response) => setHomeDetails(response.data?.message));
+  };
 
   return (
     <>
@@ -9,7 +17,9 @@ export const Home = () => {
       <br />
       <div>
         <button onClick={logout}> logout </button>
+        <button onClick={fetchDetails}> Fetch details </button>
       </div>
+      <div> {homeDetails} </div>
     </>
   );
 };
