9d224be0bc218afb47e4e75dec640fe9bbd612d9
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2025-04-01 22:07:12 +0200

    Add type field to django admin.

--- backend/authentification/admin.py
+++ backend/authentification/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from authentification.models import User
 
+UserAdmin.fieldsets += (("Custom fields set", {"fields": ("type",)}),)
+
 admin.site.register(User, UserAdmin)
