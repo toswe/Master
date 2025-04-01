7d29f8f1ff0ba2f4896b88c7e44e0a59c7827f4f
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2025-04-01 21:40:01 +0200

    Add type to User model.

--- backend/authentification/models.py
+++ backend/authentification/models.py
@@ -1,5 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    type = models.CharField(
+        max_length=20,
+        choices=[
+            ("STUDENT", "Student"),
+            ("PROFESSOR", "Professor"),
+        ],
+    )
