05094ea4a3ef1780b34ed58ff400337e6c785822
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2025-04-07 21:45:12 +0200

    Add StudentTest model.

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from backend.models import Question, Course, Test
+from backend.models import Question, Course, Test, StudentTest
 
 
 admin.site.register(Question)
 admin.site.register(Course)
 admin.site.register(Test)
+admin.site.register(StudentTest)
--- backend/backend/models.py
+++ backend/backend/models.py
@@ -3,17 +3,29 @@ from authentification.models import User
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=200)
     users = models.ManyToManyField(User, related_name="courses")
+
+    name = models.CharField(max_length=200)
 
 
 class Question(models.Model):
+    course = models.ForeignKey("Course", on_delete=models.CASCADE)
+
     question = models.TextField()
     answer = models.TextField()
-    course = models.ForeignKey("Course", on_delete=models.CASCADE)
 
 
 class Test(models.Model):
-    name = models.CharField(max_length=200)
     course = models.ForeignKey("Course", on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=200)
     questions = models.ManyToManyField("Question", related_name="tests")
+
+    is_active = models.BooleanField(default=False)
+
+
+class StudentTest(models.Model):
+    student = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    test = models.ForeignKey(Test, null=True, on_delete=models.SET_NULL)
+
+    awnsers = models.JSONField(default=list)
