2fa93b8682eec9c1c021b7a64e525b641e4ba595
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2025-05-07 22:23:49 +0200

    Create AnswerGrade model.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Local apps
     "backend",
     "authentification",
+    "grading",
 ]
 
 MIDDLEWARE = [
--- backend/grading/models.py
+++ backend/grading/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from backend.models import StudentAnswer
+
+
+class AnswerGrade(models.Model):
+    student_answer = models.ForeignKey(StudentAnswer, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    llm_response = models.JSONField(null=True, blank=True)
