bcf4179850ee2c0dcf07f868d06b79949f999974
Svetozar Ikovic <svetozar.ikovic@adaandthemachine.com>
2025-07-04 12:22:30 +0200

    Read instructions from test configuration when grading.

--- backend/grading/grader.py
+++ backend/grading/grader.py
@@ -1,15 +1,20 @@
 from grading.integrations import openai
-from backend.models import StudentAnswer
+from backend.models import StudentAnswer, Test, StudentTest
 from grading.models import AnswerGrade
 
 openai_grader = openai.OpenAI()
 
 
-def _grade_answer(answer):
-    instructions = """
+def _grade_answer(answer, instructions=None):
+    if not instructions:
+        instructions = """
 Ti si profesor visokog obrazovanja i treba da oceniš odgovor studenta na postavljeno pitanje.
 Dobijaš odgovor studenta i pitanje na koje je student odgovarao, kao i tačan odgovor.
-Na osnovu toga treba da oceniš odgovor studenta i da odgovoriš na sledeći način:
+""".strip()
+
+    instructions += """
+
+Tvoj odgovor treba da bude ocena studentskog odgovora u sledećem formatu:
 Prvi red treba da sadrži samo broj na skali od 0 do 100, gde je 100 najbolja ocena a 0 najgora.
 Ostatak odgovora treba da sadrži obrazloženje ocene.
 """
@@ -44,13 +49,15 @@ Odgovor studenta:
 
 def grade_student_test(student_test_id):
     student_answers = StudentAnswer.objects.filter(test=student_test_id)
+    student_test = StudentTest.objects.get(id=student_test_id)
 
     for answer in student_answers:
-        _grade_answer(answer)
+        _grade_answer(answer, **student_test.test.configuration)
 
 
 def grade_test(test_id):
     student_answers = StudentAnswer.objects.filter(test__test_id=test_id)
+    test = Test.objects.get(id=test_id)
 
     for answer in student_answers:
-        _grade_answer(answer)
+        _grade_answer(answer, **test.configuration)
