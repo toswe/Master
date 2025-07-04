d66c257c05355320ff87217fedec8f71eeb35b2e
Svetozar Ikovic <svetozar.ikovic@adaandthemachine.com>
2025-07-04 12:35:18 +0200

    Extract grading prompt strings to global variables.

--- backend/grading/grader.py
+++ backend/grading/grader.py
@@ -4,37 +4,44 @@ from grading.models import AnswerGrade
 
 openai_grader = openai.OpenAI()
 
-
-def _grade_answer(answer, instructions=None, **kwargs):
-    if not instructions:
-        instructions = """
+DEFAULT_INSTRUCTIONS = """
 Ti si profesor visokog obrazovanja i treba da oceniš odgovor studenta na postavljeno pitanje.
 Dobijaš odgovor studenta i pitanje na koje je student odgovarao, kao i tačan odgovor.
 """.strip()
-
-    instructions += """
+SCORING_INSTRUCTIONS = """
 
 Tvoj odgovor treba da bude ocena studentskog odgovora u sledećem formatu:
 Prvi red treba da sadrži samo broj na skali od 0 do 100, gde je 100 najbolja ocena a 0 najgora.
 Ostatak odgovora treba da sadrži obrazloženje ocene.
-"""
 
-    prompt = f"""
+"""
+QUESTION_WRAPPER = """
 Pitanje:
 ```
-{answer.question_text}
-```
-
-Tačan odgovor:
-```
-{answer.question.answer}
-```
-
-Odgovor studenta:
-```
-{answer.answer}
+{question_text}
 ```
 """
+CORRECT_ANSWER_WRAPPER = """
+Tačan odgovor:
+```
+{correct_answer}
+```
+"""
+STUDENT_ANSWER_WRAPPER = """
+Odgovor studenta:
+```
+{student_answer}
+```
+"""
+
+
+def _grade_answer(answer, instructions=DEFAULT_INSTRUCTIONS, **kwargs):
+    instructions += SCORING_INSTRUCTIONS
+    prompt = [
+        QUESTION_WRAPPER.format(question_text=answer.question_text),
+        CORRECT_ANSWER_WRAPPER.format(correct_answer=answer.question.answer),
+        STUDENT_ANSWER_WRAPPER.format(student_answer=answer.answer),
+    ].join("\n")
 
     result = openai_grader.prompt(prompt, instructions=instructions, **kwargs)
     score = int(result.output[0].content[0].text.split("\n")[0])
